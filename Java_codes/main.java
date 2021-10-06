ea617dcc604b5dddae2d6bb65e9678f7fce65bf4
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2021-10-06 12:12:55 +0100

    Add files via upload

---
+++ Java_codes/main.java
@@ -0,0 +1,20 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+/*
+Name= STARK
+surname=you
+*/
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Online IDE!! Happy Coding :)");
+        String str = "Cedric";
+        System.out.println(str);//this is a comment
+    }
+}
