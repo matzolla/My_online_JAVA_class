c85b33bb1d66d50caef6c9d04b21c0d03cc1011a
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2021-10-08 16:37:19 +0100

    Add files via upload

---
+++ Java_codes/arithmetic_solution.java
@@ -0,0 +1,26 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler 
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+/* 
+typage des variables 
+type nom=valeur 
+*/
+
+public class Main
+{ 
+    public static void main(String[] args) {
+        
+    //double pelagie=0.0;
+      //  int penda=(int) pelagie;
+      
+      /*operation 
+       type var= var_1 (operator) var_2 */
+       double penda=4+5;
+        System.out.println(penda);
+        System.out.println("Welcome to Online IDE!! Happy Coding :)");
+    }
+}
