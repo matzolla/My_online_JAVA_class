987faaa5e0b7f0d4640e3a7f7d83043e58c3e8c9
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2021-10-13 12:14:27 +0100

    Add files via upload

---
+++ Java_codes/loops.java
@@ -0,0 +1,25 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+
+public class Main
+{
+    public static void main(String[] args) {
+        int x=0;
+        if (x==3) { 
+        System.out.println("Welcome to Online IDE!! Happy Coding :)");}
+    
+else{
+        System.out.println("Welcome to Online IDE!! Happy");
+        }
+        
+        do{
+            System.out.println(x);
+            x++;
+        }
+        while(x<5);
+}
+}
